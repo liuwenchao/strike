6e901ad588ddb0dccd5391586d4705e9a834e84c
Louis Liu <louis.wenchao.liu@gmail.com>
2015-07-23 14:15:53 +0800

    add sample links of oauth.

--- src/oauth.php
+++ src/oauth.php
@@ -24,7 +24,9 @@ function post_json($url, $post_data) {
 $APP_ID = 'wx6bcdccdd8918f357';
 $APP_SC = '14ddc63ca4beea033f67e4b894717735';
 
+// https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6bcdccdd8918f357&redirect_uri=http://test.wx.zhaomw.cn/oauth.php&response_type=code&scope=snsapi_base &state=123#wechat_redirect
 if ($code = $_GET('code')) {
+  // https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx6bcdccdd8918f357&secret=14ddc63ca4beea033f67e4b894717735&code=xxx&grant_type=authorization_code
   $json = get_json("https://api.weixin.qq.com/sns/oauth2/access_token?appid=$APP_ID&secret=$APP_SC&code=$code&grant_type=authorization_code");
   if ($json['openid']) {
     post_json("http://test.account.zhaomw.cn/login", array("weixin_id" => $json->openid, "_format" => "json"));
