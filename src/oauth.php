9d89250bfbdb5c7ce877d458534394b249baefc6
Louis Liu <louis.wenchao.liu@gmail.com>
2015-07-24 08:29:16 +0800

    fix oauth.php

--- src/oauth.php
+++ src/oauth.php
@@ -2,7 +2,7 @@
 
 function get_json($url) {
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, url);
+    curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_HEADER, 0);
     $output = curl_exec($ch);
@@ -24,13 +24,19 @@ function post_json($url, $post_data) {
 $APP_ID = 'wx6bcdccdd8918f357';
 $APP_SC = '14ddc63ca4beea033f67e4b894717735';
 
-// https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6bcdccdd8918f357&redirect_uri=http://test.wx.zhaomw.cn/oauth.php&response_type=code&scope=snsapi_base &state=123#wechat_redirect
-if ($code = $_GET('code')) {
-  // https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx6bcdccdd8918f357&secret=14ddc63ca4beea033f67e4b894717735&code=xxx&grant_type=authorization_code
+// sample: https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6bcdccdd8918f357&redirect_uri=http://test.wx.zhaomw.cn/oauth.php&response_type=code&scope=snsapi_base &state=123#wechat_redirect
+if ($code = $_GET['code']) {
+  // echo $code; die();
+
+  // sample: https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx6bcdccdd8918f357&secret=14ddc63ca4beea033f67e4b894717735&code=xxx&grant_type=authorization_code
   $json = get_json("https://api.weixin.qq.com/sns/oauth2/access_token?appid=$APP_ID&secret=$APP_SC&code=$code&grant_type=authorization_code");
-  if ($json['openid']) {
-    post_json("http://test.account.zhaomw.cn/login", array("weixin_id" => $json->openid, "_format" => "json"));
-    http_redirect('/?r=register', array('weixin_id'=>$json['openid']), true, HTTP_REDIRECT_PERM);
+  if ($openid = @$json->openid) {
+    post_json("http://test.account.zhaomw.cn/login", array("weixin_id" => $openid, "_format" => "json"));
+
+    // echo "openid: $openid"; die();
+    header("Location: /index.html?r=register&weixin_id=$openid");
+    exit();
   }
 }
-http_redirect('/');
+// var_dump($json); die();
+header("Location: /index.html");
